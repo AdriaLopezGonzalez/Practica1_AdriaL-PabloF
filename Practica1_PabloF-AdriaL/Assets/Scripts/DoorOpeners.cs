73b98d6c6b526c3171b4559f604f61e029697986
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2023-10-25 21:10:22 +0200

    a

--- Practica1_PabloF-AdriaL/Assets/Scripts/DoorOpeners.cs
+++ Practica1_PabloF-AdriaL/Assets/Scripts/DoorOpeners.cs
@@ -6,15 +6,29 @@ public class DoorOpeners : MonoBehaviour
 {
     public Animation m_DoorAnimation;
     public AnimationClip m_DoorOpeningAnimationClip;
+    public AnimationClip m_DoorOpenAnimationClip;
+    public AnimationClip m_DoorClosingAnimationClip;
 
     void OpenDoor()
     {
-        m_DoorAnimation.CrossFade(m_DoorOpeningAnimationClip.name);
+        m_DoorAnimation.CrossFade(m_DoorOpeningAnimationClip.name, 0.1f);
+        m_DoorAnimation.CrossFadeQueued(m_DoorOpenAnimationClip.name, 0.1f);
+    }
+
+    void CloseDoor()
+    {
+        m_DoorAnimation.CrossFade(m_DoorClosingAnimationClip.name, 0.1f);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
            OpenDoor();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            CloseDoor();
     }
 }
